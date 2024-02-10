ced642fc610ae832e5d57bcb267a931ac71cb320
TheJJC31 <159515509+TheJJC31@users.noreply.github.com>
2024-02-10 05:38:31 +0000

    Update video_commands.py

--- video_commands.py
+++ video_commands.py
@@ -1,6 +1,5 @@
 import nextcord
 from nextcord.ext import commands
-from nextcord.ext.commands import Option
 import random
 
 class vidss(commands.Cog):
@@ -15,7 +14,7 @@ class vidss(commands.Cog):
             await ctx.send(f"An error occurred: {str(e)}")
 
     @bot.slash_command(name="video", description="Sends a video.")
-    async def video_slash(self, interaction: nextcord.Interaction, video_filename: Option(str, "The name of the video file to send")):
+    async def video_slash(self, interaction: nextcord.Interaction, video_filename: str:
         try:
             video_file = nextcord.File(f'{video_filename}', filename=f'{video_filename}')
             await interaction.response.send_message(file=video_file)
